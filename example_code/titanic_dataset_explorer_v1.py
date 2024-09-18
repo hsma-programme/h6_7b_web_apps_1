9c706e010e83630f632257748da331259ee3ab5f
Sammi Rosser <samanthajaderosser@gmail.com>
2024-09-18 16:23:52 +0100

    Update data import for simplest version of titanic dataset explorer

--- example_code/titanic_dataset_explorer_v1.py
+++ example_code/titanic_dataset_explorer_v1.py
@@ -4,28 +4,14 @@ import plotly.express as px
 
 st.title('Titanic Dataset Explorer')
 
-
 # Import data
 try:
     data = pd.read_csv("data/raw_data.csv")
 
 except FileNotFoundError:
-
-    # Download raw data:
     address = 'https://raw.githubusercontent.com/MichaelAllen1966/' + \
                 '1804_python_healthcare/master/titanic/data/train.csv'
-
     data = pd.read_csv(address)
-
-    # Create a data subfolder if one does not already exist
-    import os
-    data_directory ='./data/'
-    if not os.path.exists(data_directory):
-        os.makedirs(data_directory)
-
-    # Save data
-    data.to_csv(data_directory + 'raw_data.csv', index=False)
-
 
 # Display data
 st.dataframe(data)
